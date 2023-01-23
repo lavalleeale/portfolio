b073515cfa6ab124f57d7c31d84f4617be7a1b28
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-01-23 09:02:26 -0800

    Enable JS on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,9 +74,9 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-export const config = {
-  unstable_runtimeJS: false,
-};
+// export const config = {
+//   unstable_runtimeJS: false,
+// };
 
 Home.propTypes = {
   name: PropTypes.string.isRequired,
